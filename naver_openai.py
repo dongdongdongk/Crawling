ad75b12560466fca68cccc71a4fc079073ab02db
dhk939393 <dhk9309@gmail.com>
2024-05-09 22:46:53 +0900

    토큰 부족으로 중단

--- naver_openai.py
+++ naver_openai.py
@@ -30,8 +30,9 @@ url = f"https://search.shopping.naver.com/search/all?query={keyword}"
 driver.get(url)
 time.sleep(3)
 
-driver.find_element(By.TAG_NAME, "body").send_keys(Keys.END)
-time.sleep(1)
+for i in range(3):
+    driver.find_element(By.TAG_NAME, "body").send_keys(Keys.END)
+    time.sleep(1)
 
 html = driver.page_source
 
@@ -39,17 +40,32 @@ soup = BeautifulSoup(html, "html.parser")
 
 base_divs = soup.select("[class^=product_item]") # 원래 클래스 이름은 .product_item__MDtDF 이지만 뒤에 __ 이후 부터는 알아보기 힘들고 변경되는 경향이 있다 이때문에 product_item 으로 시작하는 클래스를 가져오란것 
 
+
+main_text_list = [] # 하나로 합친 내용을 넣을 리스트
+
 for num, base_div in enumerate(base_divs,1):
     title = base_div.select_one("[class^=product_title]")
     price = base_div.select_one("[class^=product_price]")
     detail_box_div = base_div.select_one("[class^=product_detail_box]")
+    ad_button = base_div.select_one("[class^=ad_ad_stk]")
+
+    if ad_button:
+        continue
     
-    print(num)
-    print(title.text)
-    print(price.text)
-    print(detail_box_div.text)
-    print("")
+    # print(num)
+    title_text = title.text
+    price_text = price.get_text(" ")
+    # print(title_text)
+    # print(price_text)
+   
+    prod_text = f"{title_text} | {price_text}"
+    
+    if detail_box_div:
+        detail_box_text = detail_box_div.get_text(" ")
+        prod_text += f"\n{detail_box_text}"
+        # print(detail_box_text)
+    print(prod_text)
 
+    main_text_list.append(prod_text)
 
-print(len(base_divs))
 
---
+++ openai_test.py
@@ -0,0 +1,16 @@
+from openai import OpenAI
+
+# Set your OpenAI API key
+api_key = ""
+
+client = OpenAI(api_key=api_key)
+
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": "Hello!"}
+  ]
+)
+
+print(completion.choices[0].message)
